f79899451c4cb37259fbb34b8bd824c02306339b
Toby Godfrey <toby.godfrey2003@gmail.com>
2022-03-07 22:15:39 +0000

    stuff

--- assets/js/main.js
+++ assets/js/main.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["Ismail Tasdelen.", "Security Researcher.", "Bug Bounty Hunter.", "Developer."],
+      strings: ["Toby :)"],
       typeSpeed: 100,
       loop: true,
     });
