21c7aa75ae0e3a099f0e9a87b6bcaa04917d5fcf
Toby Godfrey <toby.godfrey2003@gmail.com>
2024-12-20 12:11:32 +0000

    redesign

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,123 @@
-var i = 0;
-var txt = "Hi, I'm Toby"; /* The text */
-var speed = 100; /* The speed/duration of the effect in milliseconds */
-setInterval(blink, 4*speed);
+/*
+	Prologue by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
-function typeWriter() {
-  if (i < txt.length+1) {
-    document.getElementById("typed").innerHTML = document.getElementById("typed").innerHTML.slice(0,i) + (txt.charAt(i) + "_");
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
+(function($) {
 
-function blink(){
-  if(document.getElementById("typed").innerHTML.charAt(document.getElementById("typed").innerHTML.length-1) == "_"){
-    document.getElementById("typed").innerHTML = document.getElementById("typed").innerHTML.slice(0, document.getElementById("typed").innerHTML.length-1);
-  } else{
-    document.getElementById("typed").innerHTML += "_";
-  }
-}
+	var	$window = $(window),
+		$body = $('body'),
+		$nav = $('#nav');
 
-function ukrEng(){
-  if(document.getElementById("ukr").innerHTML === "Я знаю українську мову."){
-    document.getElementById("ukr").innerHTML = "I know Ukrainian.";
-  } else {
-    document.getElementById("ukr").innerHTML = "Я знаю українську мову.";
-  }
-  document.getElementById("ukr").blur();
-}
+	// Breakpoints.
+		breakpoints({
+			wide:      [ '961px',  '1880px' ],
+			normal:    [ '961px',  '1620px' ],
+			narrow:    [ '961px',  '1320px' ],
+			narrower:  [ '737px',  '960px'  ],
+			mobile:    [ null,     '736px'  ]
+		});
 
-function rusEng(){
-  if(document.getElementById("rus").innerHTML === "Я учил себя русскому языку."){
-    document.getElementById("rus").innerHTML = "I've taught myself Russian.";
-  } else {
-    document.getElementById("rus").innerHTML = "Я учил себя русскому языку.";
-  }
-  document.getElementById("rus").blur();
-}
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
 
-function manEng(){
-  if(document.getElementById("man").innerHTML === "现在我正在自学汉语。"){
-    document.getElementById("man").innerHTML = "Now I'm self-studying Mandarin Chinese.";
-  } else {
-    document.getElementById("man").innerHTML = "现在我正在自学汉语。";
-  }
-  document.getElementById("man").blur();
-}
+	// Nav.
+		var $nav_a = $nav.find('a');
+
+		$nav_a
+			.addClass('scrolly')
+			.on('click', function(e) {
+
+				var $this = $(this);
+
+				// External link? Bail.
+					if ($this.attr('href').charAt(0) != '#')
+						return;
+
+				// Prevent default.
+					e.preventDefault();
+
+				// Deactivate all links.
+					$nav_a.removeClass('active');
+
+				// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+					$this
+						.addClass('active')
+						.addClass('active-locked');
+
+			})
+			.each(function() {
+
+				var	$this = $(this),
+					id = $this.attr('href'),
+					$section = $(id);
+
+				// No section for this link? Bail.
+					if ($section.length < 1)
+						return;
+
+				// Scrollex.
+					$section.scrollex({
+						mode: 'middle',
+						top: '-10vh',
+						bottom: '-10vh',
+						initialize: function() {
+
+							// Deactivate section.
+								$section.addClass('inactive');
+
+						},
+						enter: function() {
+
+							// Activate section.
+								$section.removeClass('inactive');
+
+							// No locked links? Deactivate all links and activate this section's one.
+								if ($nav_a.filter('.active-locked').length == 0) {
+
+									$nav_a.removeClass('active');
+									$this.addClass('active');
+
+								}
+
+							// Otherwise, if this section's link is the one that's locked, unlock it.
+								else if ($this.hasClass('active-locked'))
+									$this.removeClass('active-locked');
+
+						}
+					});
+
+			});
+
+	// Scrolly.
+		$('.scrolly').scrolly();
+
+	// Header (narrower + mobile).
+
+		// Toggle.
+			$(
+				'<div id="headerToggle">' +
+					'<a href="#header" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
+
+		// Header.
+			$('#header')
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'header-visible'
+				});
+
+})(jQuery);
